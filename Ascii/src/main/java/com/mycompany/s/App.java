3340378a783a6b94aacfb0ab03f4a275ad6f3f92
Ahmed Shasha <119603718+ahmedshasah@users.noreply.github.com>
2022-12-13 00:48:19 +0200

    Add files via upload

---
+++ Ascii/src/main/java/com/mycompany/s/App.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.s;
+
+import java.util.Scanner;
+public class App {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        int num1;
+        char asci;
+        System.out.println("Enter the number");
+        num1 = input.nextInt();
+        asci=(char)num1;
+        System.out.println (asci);
+
+
+        
+    }
+}
